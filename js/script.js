d16bf0c59b1c049a16d5ea8259fa68b07fda82b6
KumanshF <Kumansh@Kumanshs-iMac-2.local>
2016-08-25 20:02:57 +0530

    fourth upload

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 $(document).ready(function() {
   function setHeight() {
     windowHeight = $(window).innerHeight();
-    $('#viewFirst').css('min-height', windowHeight);
-  };
+    if(windowHeight>350){
+    	$('.viewFirst').css('min-height', windowHeight);
+  	};}
   setHeight();
   
   $(window).resize(function() {
     setHeight();
   });
 
-  $('#main-content').load(MTP_ACT.html+'#content')
+ $('#main-content').load(MTP_ACT.html+'#content')
 
 });
 
@@ -26,6 +27,7 @@ $(function (){
     $(event.target).focus();
   });
 });
+
 /*---------LOADING CONTENT INTO DIV #MAIN-CONTENT-------*/
 $(document).ready(function() {
   $('#main-content').load('mtpAct.html');
@@ -57,6 +59,45 @@ $("#scrollDown").click(function() {
     }
 });
 
+/*---------------ajax-utils dynamic load-------*/
+/*
+(function (global) {
 
+var dc = {};
+
+var homeHtml = "mtpact.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
+
+
+global.$dc = dc;
+
+})(window);
+*/
 
 
